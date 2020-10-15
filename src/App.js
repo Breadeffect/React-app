1159e74e5247ec71ef8a7b56c28fce3fab84e4f6
Breadeffect <prius92@mail.ru>
2020-10-15 20:32:19 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from "./todo/TodoList";
+
+function App() {
+  return  (
+    <div className ="wrapper">
+      <h1>React tutorial</h1>
+      <TodoList></TodoList>
+    </div>
+  )
+}
+
+export default App;
